017171888f66278f11660eb0e58cc412c054bf61
Shifa Scott <shifascott@Shifas-MacBook-Pro.local>
2026-09-22 21:49:47 -1000

    v2: Sanctuary rebrand deployment - title, tiles, Sitting Intake

--- oneness-lifestyles-app/sw.js
+++ oneness-lifestyles-app/sw.js
@@ -4,7 +4,7 @@
 
 'use strict';
 
-var CACHE_NAME = 'oneness-lifestyles-shell-v20';
+var CACHE_NAME = 'oneness-lifestyles-shell-v24';
 
 // The app shell that must always be available offline.
 var SHELL = [
